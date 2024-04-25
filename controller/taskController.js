06d09c4ffb9740c42ef1f4ad1f2903e11492f850
Abenezer0923 <abnew0923@gmail.com>
2024-04-25 23:53:33 +0300

    Apriil 25

--- controller/taskController.js
+++ controller/taskController.js
@@ -33,3 +33,57 @@ exports.getTask = async (req, res) => {
         return res.status(500).json({ status:false, msg:"Internal Server error!!"})
     }
 }
+
+exports.putTask = async (req, res) => {
+    try {
+      const { description } = req.body;
+      if (!description) {
+        return res.status(400).json({ status: false, msg: "Description of task not found" });
+      }
+  
+      if (!validateObjectId(req.params.taskId)) {
+        return res.status(400).json({ status: false, msg: "Task id not valid" });
+      }
+  
+      let task = await Task.findById(req.params.taskId);
+      if (!task) {
+        return res.status(400).json({ status: false, msg: "Task with given id not found" });
+      }
+  
+      if (task.user != req.user.id) {
+        return res.status(403).json({ status: false, msg: "You can't update task of another user" });
+      }
+  
+      task = await Task.findByIdAndUpdate(req.params.taskId, { description }, { new: true });
+      res.status(200).json({ task, status: true, msg: "Task updated successfully.." });
+    }
+    catch (err) {
+      console.error(err);
+      return res.status(500).json({ status: false, msg: "Internal Server Error" });
+    }
+  }
+
+
+exports.deleteTask = async (req, res) => {
+  try {
+    if (!validateObjectId(req.params.taskId)) {
+      return res.status(400).json({ status: false, msg: "Task id not valid" });
+    }
+
+    let task = await Task.findById(req.params.taskId);
+    if (!task) {
+      return res.status(400).json({ status: false, msg: "Task with given id not found" });
+    }
+
+    if (task.user != req.user.id) {
+      return res.status(403).json({ status: false, msg: "You can't delete task of another user" });
+    }
+
+    await Task.findByIdAndDelete(req.params.taskId);
+    res.status(200).json({ status: true, msg: "Task deleted successfully.." });
+  }
+  catch (err) {
+    console.error(err);
+    return res.status(500).json({ status: false, msg: "Internal Server Error" });
+  }
+}
