67da0c6479534d991afcc0925bdb31003a6cc106
Abenezer0923 <abnew0923@gmail.com>
2024-04-25 23:51:41 +0300

    April 25

--- router/taskRouter.js
+++ router/taskRouter.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const controller = require('../controller/taskController')
 
-router.get('/createTask', controller.createTask);
-
+router.post('/createTask', controller.createTask);
+router.get('/getTask', controller.getTask);
+router.put('/putTask', controller.putTask);
+router.delete('/delete', controller.deleteTask);
 module.exports = router;
